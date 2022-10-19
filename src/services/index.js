c2f4b9e4f9f08921411ff39ca31ce910793fc428
r893803517 <18665515967@163.com>
2022-10-19 16:24:39 +0800

    Update index.js
    生日祝福

--- src/services/index.js
+++ src/services/index.js
@@ -500,7 +500,7 @@ export const getBirthdayMessage = (festivals) => {
         }
 
         if (item.diffDay === 0) {
-          message = `今天是 ${item.name} 的${age && item.isShowAge ? `${(item.useLunar ? 1 : 0) + age}岁` : ''}生日哦，祝${item.name}生日快乐！`
+          message = `今天是 ${item.name} 的${age && item.isShowAge ? `${(item.useLunar ? 1 : 0) + age}岁` : ''}生日哦，世界那么大，遇见你很不容易。而恰恰我喜欢的样子，你都有，生日快乐！`
         } else {
           message = `距离 ${item.name} 的${age && item.isShowAge ? `${age + 1}岁` : ''}生日还有${item.diffDay}天`
         }
