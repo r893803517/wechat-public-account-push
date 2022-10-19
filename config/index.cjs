eeec70505fce41da845eb03135c8cebb94f969e7
r893803517 <18665515967@163.com>
2022-10-19 11:32:15 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -21,7 +21,7 @@ const USER_CONFIG = {
       // 想要发送的人的名字
       name: 'BB',
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: '',
+      id: 'oKVAU53sVnrQfE2QN7S9UV0Dc9TI',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
       useTemplateId: 'TOLASJnrY5Sm3Jr51SGE1vUSoNagsKVEzhUy4cw-DAg',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
@@ -51,7 +51,7 @@ const USER_CONFIG = {
 
 
   // 【推送完成提醒】模板id, 用来看自己有没有发送成功的那个模板
-  CALLBACK_TEMPLATE_ID: 'template1',
+  CALLBACK_TEMPLATE_ID: 'TOLASJnrY5Sm3Jr51SGE1vUSoNagsKVEzhUy4cw-DAg',
 
   CALLBACK_USERS: [
     {
